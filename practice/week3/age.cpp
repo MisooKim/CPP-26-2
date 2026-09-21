8f0421738853470175e15e2c6f7f1d80f302ccfb
MisooKim <hanul1546@gmail.com>
2026-09-21 15:09:59 +0900

    지난주 실습 커밋

---
+++ practice/week3/age.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age;
+    cout << "나이를 입력하세요! ";
+    cin >> age;
+
+    if (age <= 12)
+        cout << "어린이"  << endl;
+    else if (age <= 19)
+        cout << "청소년" << endl;
+    else
+        cout << "성인" << endl;
+
+    return 0;    
+}
